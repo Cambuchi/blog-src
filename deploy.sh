c566ffa0b3a1c9adef05d02df34905d51804a117
Cambuchi <49548018+Cambuchi@users.noreply.github.com>
2021-12-23 04:04:02 -0800

    Updated build: 2021-12-23 04:04:02

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# If any part of the script fails the deploy stops 
+set -e
+
+# Status message
+printf "\033[0;32mDeploying updates to GitHub…\033[0m\n" 
+
+# Build the project. 
+hugo
+
+# Go To Public folder 
+cd public 
+
+# Add changes to git. 
+git add . 
+
+# Commit changes. 
+current="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Deployed site: $current"
+git commit -m "$msg"
+
+# Push source and deploy. 
+git push origin main
